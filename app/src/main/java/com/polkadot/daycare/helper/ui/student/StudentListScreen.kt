a53255202978cb04423837b16c21ea6602ef36a9
Steam Deck User <deck@Veranda.lan>
2025-02-15 16:10:11 -0800

    Remove save button from students screen

--- app/src/main/java/com/polkadot/daycare/helper/ui/student/StudentListScreen.kt
+++ app/src/main/java/com/polkadot/daycare/helper/ui/student/StudentListScreen.kt
@@ -2,30 +2,19 @@ package com.polkadot.daycare.helper.ui.student
 
 import androidx.compose.foundation.clickable
 import com.polkadot.daycare.helper.ui.theme.PolkadotAppTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -81,23 +70,6 @@ internal fun StudentsScreen(
                         StudentCardTop(it, Modifier.clickable {
                             onNavigateToStudentDetails(it.id, it.guardianId)
                         })
-                    }
-
-                    var nameStudent by remember { mutableStateOf("Compose") }
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(bottom = 24.dp),
-                        horizontalArrangement = Arrangement.spacedBy(16.dp)
-                    ) {
-                        TextField(
-                            value = nameStudent,
-                            onValueChange = { nameStudent = it }
-                        )
-
-                        Button(modifier = Modifier.width(96.dp), onClick = { onSave(nameStudent) }) {
-                            Text("Save")
-                        }
                     }
                 }
             }
